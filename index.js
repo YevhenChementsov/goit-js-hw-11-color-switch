1eeee399196d7f877a0e9f7775ff9fac67bde3bb
Yevhen Chementsov <yevhenchementsov@gmail.com>
2021-08-09 19:06:47 +0300

    Добавляет переключатель цветов

---
+++ index.js
@@ -0,0 +1,31 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const startButton = document.querySelector('[data-action="start"]');
+const stopButton = document.querySelector('[data-action="stop"]');
+
+let timerId = null;
+
+const colorSwitcher = function () {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = `${colors[randomIntegerFromInterval(0, 5)]}`
+  }, 1000);
+  startButton.removeEventListener('click', colorSwitcher);
+};
+
+startButton.addEventListener('click', colorSwitcher);
+
+stopButton.addEventListener('click', () => {
+  clearTimeout(timerId);
+  startButton.addEventListener('click', colorSwitcher);
+});
